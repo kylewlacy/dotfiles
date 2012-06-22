f767f82b712ba14ee405cc57ff877c5ac57784e5
Kyle Lacy <kylewlacy@me.com>
2012-06-22 01:39:57 -0700

    update .bash_profile to use git autocomplete

--- .bash_profile
+++ .bash_profile
@@ -1,2 +1,3 @@
 [[ -s "/Users/kyle/.rvm/scripts/rvm" ]] && source "/Users/kyle/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 export PATH="/usr/local/bin:$PATH:/usr/local/sbin"
+source ~/.scripts/bash/git-completion.bash
--- .build/build-git.sh
+++ .build/build-git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf ./git
 git clone git://github.com/git/git.git
-cp git/contrib/completion/git-completion.bash ../.scripts/bash/
 cd git
+cp contrib/completion/git-completion.bash ../../.scripts/bash/
 # TODO: Check if this line is dependent on autoconf and automake (from brew)
 make configure && ./configure
 make && make install
