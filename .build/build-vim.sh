ad1d33e46dd717a25fda97c82a7d5491ab8bb051
Kyle Lacy <kylewlacy@me.com>
2012-07-09 04:11:43 -0700

    refactor vim building code
    The standard VIM repo is now used as a fallback option; instead mvim is
    added from MacVIM. Standard VIM can be compiled with the --vim option,
    however.

--- .build/build-vim.sh
+++ .build/build-vim.sh
@@ -2,24 +2,31 @@
 breakindent=../patches/vim/breakindent/breakindent.patch
 options="--with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-perlinterp --enable-cscope"
 
-cd vim && patch -p1 < $breakindent
-cd src
-./configure $options
-make && make install && make clean
-cd ..
-patch -R -p1 < $breakindent && git clean -fd && git checkout --
-cd ..
 
 if [[ "$OSTYPE" == darwin* ]]; then
   zipfile=http://fc02.deviantart.net/fs70/f/2012/127/6/2/vim_icon_by_1024jp-d4xjn3w.zip
+  app=/Applications/MacVim.app
+  usr=/usr/local/bin
+
   cd macvim && patch -p1 < $breakindent
   cd src
   curl -o tmp.zip "$zipfile" && unzip tmp.zip VimIcon/MacVim.icns -d tmp && cp tmp/VimIcon/MacVim.icns MacVim/icons/MacVim.icns && rm -r tmp/ tmp.zip
   ./configure $options
   make
-  rm -r /Applications/MacVim.app
-  cp -R  MacVim/build/Release/MacVim.app /Applications/MacVim.app
+  rm -r "$app"
+  cp -R MacVim/build/Release/MacVim.app "$app"
+  cp MacVim/mvim "$usr/mvim"
   make clean
   cd ..
   patch -R -p1 < $breakindent && git clean -fd && git reset --hard
+fi 
+  
+if [[ "$1" == --vim ]]; then
+  cd vim && patch -p1 < $breakindent
+  cd src
+  ./configure $options
+  make && make install && make clean
+  cd ..
+  patch -R -p1 < $breakindent && git clean -fd && git reset --hard
+  cd ..
 fi
