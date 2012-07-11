7bc811a0099da5f69e29393e7f0374d54bdca40b
Kyle Lacy <kylewlacy@me.com>
2012-07-11 04:38:25 -0700

    reverted git diff tool to vimdiff (mvim doesn't work)

--- .scripts/git/diff-wrapper.sh
+++ .scripts/git/diff-wrapper.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-if [[ "$OSTYPE" == darwin* ]]; then
-  if [[ `mvim --serverlist` = "VIM" ]]; then
-    mvim -d "$2" "$5"
-  else
-    mvim -v -d "$2" "$5"
-  fi
-else
-  vimdiff "$2" "$5"
-fi
+vimdiff "$2" "$5"
